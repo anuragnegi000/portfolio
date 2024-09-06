babb0d54ce218d7a44776398175f294aefd8c007
anuragnegi000 <anurag.negi862@gmail.com>
2024-09-06 21:51:06 +0530

    deployment error fixing

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -14,9 +14,10 @@ interface BlurFadeProps {
   delay?: number;
   yOffset?: number;
   inView?: boolean;
-  inViewMargin?: string;
+  inViewMargin?: string; // Margin as a string
   blur?: string;
 }
+
 const BlurFade = ({
   children,
   className,
@@ -28,14 +29,16 @@ const BlurFade = ({
   inViewMargin = "-50px",
   blur = "6px",
 }: BlurFadeProps) => {
-  const ref = useRef(null);
-  const inViewResult = useInView(ref, { once: true, margin: inViewMargin });
+  const ref = useRef<HTMLDivElement | null>(null);
+  const inViewResult = useInView(ref, { once: true});
+
   const isInView = !inView || inViewResult;
   const defaultVariants: Variants = {
     hidden: { y: yOffset, opacity: 0, filter: `blur(${blur})` },
     visible: { y: -yOffset, opacity: 1, filter: `blur(0px)` },
   };
   const combinedVariants = variant || defaultVariants;
+  
   return (
     <AnimatePresence>
       <motion.div
